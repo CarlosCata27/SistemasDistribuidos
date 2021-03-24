41d9dd00b13ac6d31eb338b1f42f51f22377b260
Carlos Catarino <carlos_cata27@hotmail.com>
2021-03-24 11:24:00 -0600

    Servicio Web

---
+++ WebServ/app.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+
+app.get("/", async function (req, res) {
+    let r = {'dato': 1,'valor': 'Nio'};
+    res.json(r);
+});
+
+app.get("/ruta1", async function (req, res) {
+    let r = {'dato': 1,'valor': 'Nio'};
+    res.json(r);
+});
+
+app.get("/ruta2", async function (req, res) {
+    let r = {};
+    res.json(r);
+});
+
+app.get("/ruta3", async function (req, res) {
+    let r = {};
+    res.json(r);
+});
+
+app.get("/ruta4", async function (req, res) {
+    let r = {};
+    res.json(r);
+});
+
+app.get("/ruta5/subruta/todos", async function (req, res) {
+    let r = {};
+    res.json(r);
+});
+
+app.get("/ruta6/crear", async function (req, res) {
+    let r = {};
+    res.json(r);
+});
+
+app.get("/ruta7/echo", async function (req, res) {
+    let r = { texto: req.query.texto, nombre: req.query.nombre, proyecto:req.query.proyecto};
+    res.json(r);
+});
+
+app.listen(3000, function() {
+    console.log('Aplicación ejemplo, escuchando el puerto 3000!');
+});
